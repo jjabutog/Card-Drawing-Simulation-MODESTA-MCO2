89a84405ee2887221bcbec94bd6fd66576fb8693
DESKTOP-RMKDPDI\Tyler <justin_venzon@dlsu.edu.ph>
2017-08-05 20:01:54 +0800

    rJava test
    adds rJava test

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,4 @@
+import org.rosuda.JRI.Rengine;
 
 public final class Driver {
 	
@@ -8,7 +9,27 @@ public final class Driver {
 	public static void start() {
 		// TODO Auto-generated method stub
 		
+		/** Sample code for using R **/
 		
+		// Create an R vector in the form of a string.
+		String javaVector = "c(1,2,3,4,5)";
+		
+        // Start Rengine.
+		Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+		
+		// The vector that was created in JAVA context
+		// is stored in 'rVector' which is a variable in R context.
+		engine.eval("rVector=" + javaVector);
+		
+		//Calculate MEAN of vector using R syntax.
+		engine.eval("meanVal=mean(rVector)");
+		
+        //Print output values
+		double mean = engine.eval("meanVal").asDouble();
+		
+		System.out.println("Mean of given vector is=" + mean);
+		
+		/** End of Sample Code **/
 	}
 
 }
