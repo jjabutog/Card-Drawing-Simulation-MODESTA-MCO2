8c6c38706f05c71ac8eecef4d494245190108c8b
Jan Joseph Abutog <jan_abutog@dlsu.edu.ph>
2017-08-05 22:20:45 +0800

    rJava library imported

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,4 @@
+import org.rosuda.JRI.Rengine;
 
 public final class Driver {
 	
@@ -7,7 +8,23 @@ public final class Driver {
 
 	public static void start() {
 		// TODO Auto-generated method stub
-		
+		// Create an R vector in the form of a string.
+        String javaVector = "c(1,2,3,4,5)";
+
+        // Start Rengine.
+        Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+
+        // The vector that was created in JAVA context is stored in 'rVector' which is a variable in R context.
+        engine.eval("rVector=" + javaVector);
+        
+        //Calculate MEAN of vector using R syntax.
+        engine.eval("meanVal=mean(rVector)");
+        
+        //Retrieve MEAN value
+        double mean = engine.eval("meanVal").asDouble();
+        
+        //Print output values
+        System.out.println("Mean of given vector is lol=" + mean);
 		
 	}
 
