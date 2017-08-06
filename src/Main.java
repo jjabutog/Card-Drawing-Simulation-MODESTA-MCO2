7beb7dd0115ce37c986c46379cb6ff3a66fb39f5
DESKTOP-RMKDPDI\Tyler <justin_venzon@dlsu.edu.ph>
2017-08-06 14:46:48 +0800

    window and rjava experiment
    adds progress

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
+import java.util.Vector;
+
+import org.rosuda.JRI.Rengine;
+
+import model.Cards;
+import model.Deck;
 
 public class Main {
 	public static void main(String[] args){
 		
 		Driver.start();
+		
+//	    Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+//	    
+//	    
+//	    String deckVector = "c(1,2,3,4,5,6,7,8,9,10,11,12,13," +
+//	    		       "1,2,3,4,5,6,7,8,9,10,11,12,13," +
+//	    		       "1,2,3,4,5,6,7,8,9,10,11,12,13," +
+//	    		       "1,2,3,4,5,6,7,8,9,10,11,12,13)";
+//	    
+//	    String deckVector2 = "c(1,2,3,4,5,6,7,8,9,10,11,12,13)";
+//	    
+//	    
+//	    engine.eval("deckTotal=" + deckVector);
+//	    engine.eval("deckTotal2=" + deckVector2);
+//
+//	   int[] posTotal1 = engine.eval("posTotal1 <- unique(combn(deckTotal, 1, sum))").asIntArray();
+//	    
+//	    
+//	    for(int i = 0; i < posTotal1.length; i++) {
+//	    	System.out.print(posTotal1[i] + " " );
+//	    }
+//	    engine.eval("library('gtools')");
+//		/*
+//		process(numCards,nTrials,deck);
+//		*/
 		
 	}
 }
--- src/view/Window.java
+++ src/view/Window.java
@@ -2,10 +2,22 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Window {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -35,8 +47,54 @@ public class Window {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1427, 675);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(15, 395, 305, 208);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(147, 89, 121, 26);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(147, 34, 121, 26);
+		panel.add(textField_1);
+		
+		JLabel lblNoOfTrials = new JLabel("No. of Trials");
+		lblNoOfTrials.setBounds(15, 37, 92, 20);
+		panel.add(lblNoOfTrials);
+		
+		JLabel lblDesiredTotal = new JLabel("Desired Total");
+		lblDesiredTotal.setBounds(15, 92, 117, 20);
+		panel.add(lblDesiredTotal);
+		
+		JButton btnStart = new JButton("START");
+		btnStart.setBounds(89, 148, 115, 29);
+		panel.add(btnStart);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBounds(335, 0, 1070, 619);
+		frame.getContentPane().add(panel_5);
+		panel_5.setLayout(new BoxLayout(panel_5, BoxLayout.X_AXIS));
+		
+		JPanel panel_3 = new JPanel();
+		panel_5.add(panel_3);
+		
+		JPanel panel_1 = new JPanel();
+		panel_5.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_5.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_4 = new JPanel();
+		panel_5.add(panel_4);
 	}
-
 }
