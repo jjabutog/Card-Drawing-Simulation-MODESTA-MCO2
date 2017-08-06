883e88d39e651b2982aa58c9960db0814c36ba95
DESKTOP-RMKDPDI\Tyler <justin_venzon@dlsu.edu.ph>
2017-08-06 22:30:05 +0800

    get total possible
    total possible

--- src/view/Window.java
+++ src/view/Window.java
@@ -1,6 +1,15 @@
 package view;
 import java.awt.EventQueue;
 
+import java.util.Arrays;
+import java.util.Vector;
+
+import org.rosuda.JRI.Rengine;
+import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.RList;
+import org.rosuda.JRI.RVector;
+
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -499,6 +508,135 @@ public class Window {
 		}
 		
 		return total;
+	}
+	
+	private int[] computeTotalPossibleSums(int numDraws, boolean wReplacement) {
+	   
+		String deckVector = "c(1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6,7,8,9,10,11,12,13)";
+	    		     
+	    
+	    String deckVector2 = "c(1,2,3,4,5,6,7,8,9,10,11,12,13)";
+	    
+	    Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+
+	    
+	    engine.eval("deckTotal=" + deckVector);
+	    engine.eval("deckTotal2=" + deckVector2);
+
+	    /* Initialize all possible card value sums without replacement */
+	    
+	    REXP posTotal1 = engine.eval("posTotal1 <- unique(combn(deckTotal, 1, sum))");
+	    double [] posTotal1Double = posTotal1.asDoubleArray();
+	    	    
+	    int [] intPosTotal1 = new int[posTotal1Double.length];
+	    for(int i = 0; i < intPosTotal1.length; i++) {
+	    	intPosTotal1[i] = (int) posTotal1Double[i];
+//	    	System.out.println(intPosTotal1[i] );
+	    }
+	    
+	    REXP posTotal2 = engine.eval("posTotal2 <- unique(combn(deckTotal, 2, sum))");
+	    double [] posTotal2Double = posTotal2.asDoubleArray();
+	    	    
+	    int [] intPosTotal2 = new int[posTotal2Double.length];
+	    for(int i = 0; i < intPosTotal2.length; i++) {
+	    	intPosTotal2[i] = (int) posTotal2Double[i];
+//	    	System.out.println(intPosTotal2[i] );
+	    }
+	    
+	    REXP posTotal3 = engine.eval("posTotal3 <- unique(combn(deckTotal, 3, sum))");
+	    double [] posTotal3Double = posTotal3.asDoubleArray();
+	    	    
+	    int [] intPosTotal3 = new int[posTotal3Double.length];
+	    for(int i = 0; i < intPosTotal3.length; i++) {
+	    	intPosTotal3[i] = (int) posTotal3Double[i];
+//	    	System.out.println(intPosTotal3[i] );
+	    }
+	    
+	    REXP posTotal4 = engine.eval("posTotal4 <- unique(combn(deckTotal, 4, sum))");
+	    double [] posTotal4Double = posTotal4.asDoubleArray();
+	    	    
+	    int [] intPosTotal4 = new int[posTotal4Double.length];
+	    for(int i = 0; i < intPosTotal4.length; i++) {
+	    	intPosTotal4[i] = (int) posTotal4Double[i];
+//	    	System.out.println(intPosTotal4[i] );
+	    }
+	    
+	    REXP posTotal5 = engine.eval("posTotal5 <- unique(combn(deckTotal, 5, sum))");
+	    double [] posTotal5Double = posTotal5.asDoubleArray();
+	    	    
+	    int [] intPosTotal5 = new int[posTotal5Double.length];
+	    for(int i = 0; i < intPosTotal5.length; i++) {
+	    	intPosTotal5[i] = (int) posTotal5Double[i];
+//	    	System.out.println(intPosTotal5[i] );
+	    }
+	    Arrays.sort(intPosTotal1);
+	    Arrays.sort(intPosTotal2);
+	    Arrays.sort(intPosTotal3);
+	    Arrays.sort(intPosTotal4);
+	    Arrays.sort(intPosTotal5);
+
+	    /* Initialize all possible card value sums with replacement */
+
+	   
+    	    
+	    int [] intPosTotal1Rep = new int[14];
+	    for(int i = 1; i < intPosTotal1Rep.length; i++) {
+	    	intPosTotal1Rep[i] = i;
+	    }
+	    
+	    int [] intPosTotal2Rep = new int[27];
+	    for(int i = 2; i < intPosTotal2Rep.length; i++) {
+	    	intPosTotal2Rep[i] = i;
+	    }
+	    
+	    int [] intPosTotal3Rep = new int[40];
+	    for(int i = 3; i < intPosTotal3Rep.length; i++) {
+	    	intPosTotal3Rep[i] = i;
+	    }
+	    
+	    int [] intPosTotal4Rep = new int[53];
+	    for(int i = 4; i < intPosTotal4Rep.length; i++) {
+	    	intPosTotal4Rep[i] = i;
+	    }
+	    
+	    int [] intPosTotal5Rep = new int[66];
+	    for(int i = 5; i < intPosTotal5Rep.length; i++) {
+	    	intPosTotal5Rep[i] = i;
+	    }
+	    
+	    if(numDraws == 1 && wReplacement == false) {
+	    	return intPosTotal1;
+	    }
+	    else if(numDraws == 1 && wReplacement == true) {
+	    	return intPosTotal1Rep;
+	    }
+	    else if(numDraws == 2 && wReplacement == false) {
+	    	return intPosTotal2;
+	    }
+	    else if(numDraws == 2 && wReplacement == true) {
+	    	return intPosTotal2Rep;
+	    }
+	    else if(numDraws == 3 && wReplacement == false) {
+	    	return intPosTotal3;
+	    }
+	    else if(numDraws == 3 && wReplacement == true) {
+	    	return intPosTotal3Rep;
+	    }
+	    else if(numDraws == 4 && wReplacement == false) {
+	    	return intPosTotal4;
+	    }
+	    else if(numDraws == 4 && wReplacement == true) {
+	    	return intPosTotal4Rep;
+	    }
+	    else if(numDraws == 5 && wReplacement == false) {
+	    	return intPosTotal5;
+	    }
+	    else if(numDraws == 5 && wReplacement == true) {
+	    	return intPosTotal5Rep;
+	    }
+	
+	    return intPosTotal5Rep;
+		
 	}
 
 
